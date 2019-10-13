218bdb932999cafaefb4e2d3d4290a2b1b3937e8
ReCen20 <alpha55757@gmail.com>
2019-10-13 23:57:32 +0400

    pre-alpha 5 (css-config part 3)

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
             {
                 loader:'css-loader',
                 options: { sourceMap: true }
+            },
+            {
+                loader: 'postcss-loader',
+                options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
             }
         ],
       },
