779dfd728ccba05a7a35d3b5fe42951e37537132
ReCen20 <alpha55757@gmail.com>
2019-10-16 22:29:51 +0400

    pre-alpha 6 (webpack-merge on 3 config files)

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,9 +10,6 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist'
   },
-  devServer: {
-    overlay: true
-  },
   module: {
     rules: [{
         test: /\.js$/,
